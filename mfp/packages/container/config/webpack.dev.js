f374cfc013593fa0b848661a4ef4e39bbf95316c
kcabhish <kcabhish@gmail.com>
2025-05-11 06:10:17 -0400

    feat: setting up the container(HOST) app

---
+++ mfp/packages/container/config/webpack.dev.js
@@ -0,0 +1,22 @@
+// This allows to merge different webpack configs
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
+
+// Prioritizes configs right to left on the parameters
+module.exports = merge(commonConfig, devConfig);
