0a7385775267c054b311538850d6ce6835dfeea6
kcabhish <kcabhish@gmail.com>
2025-05-11 22:25:08 -0400

    feat: setting up webpack.prod.js for marketing and container

---
+++ mfp/packages/marketing/config/webpack.prod.js
@@ -0,0 +1,28 @@
+// This allows to merge different webpack configs
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+// environment variable to load the AWS resource
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+// Prioritizes configs right to left on the parameters during merge
+module.exports = merge(commonConfig, prodConfig);
---
+++ notes/4-ImplementingCICD.md
@@ -0,0 +1,9 @@
+# Implementing a CI/CD pipeline
+
+## Requirements Around Deployment.
+
+- deploy each microfrontend independently (including the container)
+- locationof the child app remoteEntry.js files must be known at build time.
+- Many front-end deployment solutions assume you are deploying a single project, we need something that can handle multiple different ones.
+- Need a CI/CD pipeline of some sort.
+- the remoteEntry.js file name is fixed! Need to think about caching issues.
